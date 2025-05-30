172383517c99657608063387c7b70286c46ef859
James Nathan Belue <computerghost@hotmail.com>
2025-05-30 02:18:46 -0500

    #5 - Add configuration page

---
+++ src/CMS/Controllers/ConfigurationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers;
+
+[Route("/configuration/")]
+public class ConfigurationController : Controller
+{
+    [HttpGet]
+    public IActionResult Edit()
+    {
+        return View();
+    }
+}
