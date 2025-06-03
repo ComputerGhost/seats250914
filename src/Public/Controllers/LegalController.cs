244da2ecb9170bd2a72239afe790f4f7803aec87
James Nathan Belue <computerghost@hotmail.com>
2025-06-03 00:29:18 -0500

    #6 - public website pages, except reservations; scss build; started this week's report

---
+++ src/Public/Controllers/LegalController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Public.Controllers;
+
+[Route("/legal/")]
+public class LegalController : Controller
+{
+    [HttpGet("privacy-policy")]
+    public IActionResult PrivacyPolicy()
+    {
+        return View();
+    }
+
+    [HttpGet("terms-of-service")]
+    public IActionResult TermsOfService()
+    {
+        return View();
+    }
+}
