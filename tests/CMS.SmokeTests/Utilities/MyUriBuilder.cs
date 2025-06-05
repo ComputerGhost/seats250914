b68e8a891875aa6c1a8c0ee4fe54cb72304dada7
James Nathan Belue <computerghost@hotmail.com>
2025-06-05 14:33:48 -0500

    #33 - automated smoke tests to replace most manual tests

---
+++ tests/CMS.SmokeTests/Utilities/MyUriBuilder.cs
@@ -0,0 +1,29 @@
+﻿namespace CMS.SmokeTests.Utilities;
+internal class MyUriBuilder(string uri) : UriBuilder(uri)
+{
+    private string? _subdomain;
+
+    public MyUriBuilder WithSubdomain(string subdomain)
+    {
+        _subdomain = subdomain;
+        return this;
+    }
+
+    public MyUriBuilder WithScheme(string scheme)
+    {
+        Scheme = scheme;
+        return this;
+    }
+
+    public override string ToString()
+    {
+        Port = (Scheme == "https") ? 443 : 80;
+
+        if (!string.IsNullOrEmpty(_subdomain))
+        {
+            Host = _subdomain + "." + Host;
+        }
+
+        return base.ToString();
+    }
+}
---
+++ tests/Public.SmokeTests/Tests/KoreanLocalizationTests.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+
+namespace Public.SmokeTests.Tests;
+
+[TestClass]
+public sealed class KoreanLocalizationTests : TestBase
+{
+    private IWebDriver Driver { get; set; } = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        Driver = CreateDriver("ko");
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        Driver.Quit();
+        Driver.Dispose();
+    }
+
+    [TestMethod]
+    public void WhenVisitingWebsiteForFirstTime_WebsiteRendersInEnglish()
+    {
+        // Arrange
+
+        // Act
+        Driver.Navigate().GoToUrl(TargetUrl);
+
+        // Assert
+        Assert.AreEqual("혜린 팬미팅 2025", Driver.Title);
+    }
+
+    [TestMethod]
+    public void WhenKoreanSelected_WebsiteRendersInKorean()
+    {
+        // Arrange
+        Driver.Navigate().GoToUrl(TargetUrl);
+        var switcher = Driver.FindElement(By.Id("culture-switcher"));
+        var dropdown = switcher.FindElement(By.ClassName("dropdown-toggle"));
+        dropdown.Click();
+        var koreanButton = switcher.FindElement(By.XPath(".//button[text()='English']"));
+        koreanButton.Click();
+
+        // Act
+        Driver.Navigate().GoToUrl(TargetUrl);
+
+        // Assert
+        Assert.AreEqual("Hyelin Fanmeeting 2025", Driver.Title);
+    }
+}
