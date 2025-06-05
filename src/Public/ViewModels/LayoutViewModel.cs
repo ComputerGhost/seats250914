a16cd172e370662fea92b404cf38a0976c7e6de9
James Nathan Belue <computerghost@hotmail.com>
2025-06-04 22:11:03 -0500

    #6 - Finally got all the new pages pretty (in laptop browser)

---
+++ src/Public/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Public.ViewModels;
+
+public class LayoutViewModel
+{
+    /*
+     * Shared views like '_Layout.cshtml' cannot use an `IViewLocalizer`, because 
+     * that option only works when the file matches the route. Instead, a generic 
+     * localizer such as `IStringLocalizer<>` must be used. This file provides the 
+     * required type parameter for that.
+     * 
+     * It doesn't matter that there is nothing in this class.
+     */
+}
