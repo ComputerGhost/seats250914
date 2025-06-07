40f66a7d5f14f45972eb9df05ac64646b6cbc5a5
James Nathan Belue <computerghost@hotmail.com>
2025-06-07 11:54:53 -0500

    #28 - Time stamps are now displayed in local time. They can still be converted, but I reduced the number of clicks to do this so it's a little cleaner.

--- src/CMS/wwwroot/js/global.js
+++ src/CMS/wwwroot/js/global.js
@@ -1,58 +1,40 @@
 ﻿/* This file requires Bootstrap, jQuery, and Luxon. */
 
-// All of the text to be translated here:
-const CONVERT_TIME_TEXT = "Convert time";
-
 // Moving library access to the top so all of VS' false errors are in one place.
-var DateTime = luxon.DateTime;
-var createModal = (id) => new bootstrap.Modal(document.getElementById(id));
+const DateTime = luxon.DateTime;
+const createModal = (id) => new bootstrap.Modal(document.getElementById(id));
 
 /*
  * Time conversion feature
  */
 $(document).ready(() => {
-    var timezoneModal = createModal("timezone-converter-modal");
-    var $timestampDisplay = $("#timezone-converter-timestamp");
-    var $zonedTimeDisplay = $("#timezone-converter-zonedtime");
-    var $timezoneSelect = $("#timezone-converter-timezone");
+    const timeZones = Intl.supportedValuesOf("timeZone");
+    const currentTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    function updateZonedTime() {
-        var timezone = $timezoneSelect.find("option:selected").text();
-        if (timezone.length == 0) {
-            $zonedTimeDisplay.text("");
-            return;
-        }
-
-        var utcTime = DateTime.fromISO($timestampDisplay.text());
-        var zonedTime = utcTime.setZone(timezone);
-        var displayTime = zonedTime.toFormat("yyyy-MM-dd HH:mm:ss");
-        $zonedTimeDisplay.text(displayTime);
-    }
-
-    $(Intl.supportedValuesOf("timeZone")).each((_, item) => {
-        $timezoneSelect.append($("<option>", { text: item }));
+    const options = timeZones.map((timezone) => {
+        return $("<option>").val(timezone).text(timezone);
     });
-
-    $timezoneSelect.on("change", updateZonedTime);
 
     $("time").each(function () {
-        var $subject = $(this);
+        const $time = $(this);
+        const $select = $("<select>")
+            .addClass("form-select")
+            .attr("aria-label", "Change displayed time zone.");
 
-        var $convertButton = $("<button>");
-        $convertButton.attr(
-            "class",
-            "convert-time-button btn btn-sm btn-outline-secondary"
-        );
-        $convertButton.append($("<i class='bi bi-globe me-2'>"));
-        $convertButton.append(CONVERT_TIME_TEXT);
+        options.forEach((option) => $select.append(option.clone()));
 
-        $convertButton.on("click", () => {
-            $timestampDisplay.text($subject.attr("datetime"));
-            updateZonedTime();
-            timezoneModal.toggle();
-        });
+        $select.on("change", () => updateDisplayTime($time, $select.val()));
 
-        $subject.after($convertButton);
+        $select.val(currentTimeZone);
+        updateDisplayTime($time, currentTimeZone);
+
+        $time.parent().append($select);
     });
-});
 
+    function updateDisplayTime($time, timeZone) {
+        const utcTime = DateTime.fromISO($time.attr("datetime"));
+        const zonedTime = utcTime.setZone(timeZone);
+        const displayTime = zonedTime.toFormat("yyyy-MM-dd HH:mm:ss");
+        $time.text(displayTime);
+    }
+});
