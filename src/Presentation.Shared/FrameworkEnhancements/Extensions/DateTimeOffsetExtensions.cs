919eeb80ee435c8bbd1d97aec3fc4c677e6bb40b
James Nathan Belue <computerghost@hotmail.com>
2025-07-12 12:10:44 -0500

    #250 - fix server timezone id difference

--- src/Presentation.Shared/FrameworkEnhancements/Extensions/DateTimeOffsetExtensions.cs
+++ src/Presentation.Shared/FrameworkEnhancements/Extensions/DateTimeOffsetExtensions.cs
@@ -3,7 +3,8 @@ public static class DateTimeOffsetExtensions
 {
     public static string ToNormalizedString(this DateTimeOffset when, string timeZone = "Korea Standard Time")
     {
-        if (timeZone == "Korea Standard Time")
+        // My local computer uses "Korea Standard Time", but the server uses "Asia/Seoul".
+        if (timeZone is "Korea Standard Time" or "Asia/Seoul")
         {
             var tz = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
             var localTime = TimeZoneInfo.ConvertTime(when, tz);
